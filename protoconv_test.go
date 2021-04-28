284b34b19c4f9b63cd4b548c45ff873eb181b800
Duoc Nguyen <nguyenvanduocit@gmail.com>
2021-04-28 11:10:45 +0700

    Update protoconv_test.go

--- protoconv_test.go
+++ protoconv_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 
 	"github.com/golang/protobuf/proto"
-	"github.com/nguyenvanduocit/ptconv/testdata"
+	"github.com/nguyenvanduocit/pbconv/testdata"
 )
 
 var fooMessage = &testdata.FooMessage{
